218d4c1cfe1e1d89695142b97e051fe5dd55b8b8
Roar Bakken Stovner <robast@oslomet.no>
2025-12-11 21:13:15 +0100

    Add script to run UiO harvest process
    Former-commit-id: 91ba343c1a7fa192792632f228c0c379211d8608

---
+++ R/run_harvest_uio.R
@@ -0,0 +1,43 @@
+library(dplyr)
+
+source("R/utils.R")
+source("R/fetch_html_cols.R")
+source("R/extract_fulltext.R")
+source("R/add_course_url.R")
+source("R/checkpoint.R")
+
+courses <- readRDS("data/courses.RDS")
+
+uio_all <- courses |>
+  filter(
+    institution_short == "uio",
+    Årstall %in% c(2017)
+  ) |>
+  add_course_id() |>
+  validate_courses("initial") |>
+  add_course_url() |>
+  validate_courses("with_url")
+
+# UiO does not store old versions of their Emneplan, so you only need the distinct urls created.
+# This makes historical UiO data rather limited, and looking up their Emneplan might give the wrong result.
+# I therefore only keep the latest version of each individual url.
+uio <- uio_all |>
+  slice_max(Årstall, n = 1, with_ties = FALSE, by = "url")
+
+uio <- fetch_html_with_checkpoint(
+  uio,
+  checkpoint_path = "data/checkpoint/html_uio.RDS"
+)
+
+uio$fulltext <- extract_fulltext(uio$institution_short, uio$html)
+
+# Join the fulltexts that was fetched into the full course tibble.
+uio <- uio |>
+  select(course_id, html, html_error, html_success, fulltext) |> 
+  right_join(uio_all, by = "course_id") |> 
+  relocate(contains("html"), fulltext, .after = last_col())
+
+saveRDS(
+  uio,
+  file = "data/html_uio.RDS"
+)
